8cf15c5862d4e9254bc146896d17773e9879becd
Vibilan <vibilan2793@gmail.com>
2026-05-31 14:45:16 +0530

    4/12

--- miniExpress.js
+++ miniExpress.js
@@ -2,7 +2,18 @@ function miniExpress() {
     const middlewares = [];
     const routes = [];
 
-    return {};
-}
+    return {
+        use(fn) {
+            middlewares.push(fn);
+        },
+        get(route, handler) {
+            routes.push({
+                method: "GET",
+                route,
+                handler
+            });
+        }
+    };
+};
 
 module.exports = miniExpress;
