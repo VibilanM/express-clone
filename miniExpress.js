307ad780076a950ce02cb4a9fe28a1f8093887a7
Vibilan <vibilan2793@gmail.com>
2026-05-31 16:36:19 +0530

    6/12

--- miniExpress.js
+++ miniExpress.js
@@ -2,18 +2,33 @@ function miniExpress() {
     const middlewares = [];
     const routes = [];
 
-    return {
-        use(fn) {
-            middlewares.push(fn);
-        },
-        get(route, handler) {
-            routes.push({
-                method: "GET",
-                route,
-                handler
-            });
+    function app(req, res) {
+        const route = routes.find((r) => {
+            return r.method === req.method && r.path === req.url;
+        });
+
+        if (route) {
+            route.handler(req, res);
         }
-    };
+        else {
+            res.statusCode = 404;
+            return res.end("404 NOT FOUND.")
+        }
+    }
+
+    app.use = function (fn) {
+        middlewares.push(fn);
+    }
+
+    app.get = function (path, handler) {
+        routes.push({
+            method: "GET",
+            path,
+            handler
+        });
+    }
+
+    return app;
 };
 
 module.exports = miniExpress;
--- test.js
+++ test.js
@@ -1,5 +1,19 @@
+const http = require("http");
+
 const miniExpress = require("./miniExpress.js");
 
 const app = miniExpress();
 
-console.log(app);
+app.get("/", (req, res) => {
+    res.end("Home Route");
+});
+
+app.get("/users", (req, res) => {
+    res.end("Users Route");
+});
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+    console.log("Server is running in PORT 3000");
+});
